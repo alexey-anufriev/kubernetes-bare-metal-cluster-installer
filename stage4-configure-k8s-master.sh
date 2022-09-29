10818a61eafdebda82873c571372f01163a67ad4
alexey-anufriev <contact@alexey-anufriev.com>
2022-09-29 02:08:32 +0200

    chore: add logs

--- stage4-configure-k8s-master.sh
+++ stage4-configure-k8s-master.sh
@@ -11,15 +11,19 @@ OBSERVABILITY_STACK_NODE_SELECTOR=$7
 if [[ "$MODE" == "master" ]]; then
     # Configure k8s on master
     kubeadm init --pod-network-cidr=10.244.0.0/16
+    info_log "master node initialized"
 
+    # Allow root to control the cluster
     mkdir -p $HOME/.kube
     cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
     chown $(id -u):$(id -g) $HOME/.kube/config
 
     # Configure network
     kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+    info_log "network set"
 
     # Verify setup
+    info_log "checking cluster state"
     kubectl get all --all-namespaces
 
     # Notify workers to join
